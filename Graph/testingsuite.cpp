331a568af52281b421996b5a0dbbac21b01afccf
z-zarif <rahatzarif0443@gmail.com>
2026-03-31 19:57:44 +0600

    added gitignore

---
+++ Graph/testingsuite.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+void fastIO()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+void print(string a)
+{
+    cout << a << endl;
+}
+int main()
+{
+    fastIO();
+    print("hellonigga this is my first time typing in my own mechanical keyboard");
+    
+
+    return 0;
+}
